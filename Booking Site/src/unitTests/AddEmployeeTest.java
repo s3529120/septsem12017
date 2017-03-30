8c7096c1c79ca4e0a30829fece70924aebcd48ee
RobLW <s3433096@student.rmit.edu.au>
2017-03-30 23:25:01 +1100

    Added method to test check employee

--- Booking Site/src/unitTests/AddEmployeeTest.java
+++ Booking Site/src/unitTests/AddEmployeeTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import Controller.DatabaseController;
 import Controller.EmployeeController;
 import Model.DatabaseModel;
+import Model.EmployeeModel;
 
 public class AddEmployeeTest {
 
@@ -26,30 +27,35 @@ public class AddEmployeeTest {
 		DatabaseModel mod = new DatabaseModel();
 		DatabaseController cont = new DatabaseController(mod);
 		String sql;
+
+		cont.createConnection();
+		sql="DROP TABLE IF EXISTS Employee;";
+		cont.prepareStatement(sql);
+		cont.runSQLUpdate();
+
+		sql="DROP TABLE IF EXISTS Address;";
+		cont.prepareStatement(sql);
+		cont.runSQLUpdate();
+
+		sql="CREATE TABLE Address("
+				+ "EmployeeEmail TEXT NOT NULL, "
+				+ "StreetAddress TEXT NOT NULL, "
+				+ "City TEXT NOT NULL, " 
+				+ "State TEXT NOT NULL, "
+				+ "PostCode TEXT NOT NULL, "
+				+ "PRIMARY KEY (EmployeeEmail));";
+		cont.prepareStatement(sql);
+		cont.runSQLUpdate();
+
+		sql="CREATE TABLE Employee("
+				+ "Name TEXT NOT NULL, "
+				+ "ContactNo TEXT NOT NULL, "
+				+ "Email TEXT NOT NULL, "
+				+ "PRIMARY KEY (Email));";
+		cont.prepareStatement(sql);
+		cont.runSQLUpdate();
+
 		try{
-			cont.createConnection();
-			sql="DROP TABLE IF EXISTS Employee; DROP TABLE IF EXISTS Address;";
-			cont.prepareStatement(sql);
-			cont.runSQLUpdate();
-
-			sql="CREATE TABLE Employee("
-					+ "Name TEXT NOT NULL, "
-					+ "ContactNo TEXT NOT NULL, "
-					+ "Email TEXT NOT NULL, "
-					+ "PRIMARY KEY (Email));";
-			cont.prepareStatement(sql);
-			cont.runSQLUpdate();
-
-			sql="CREATE TABLE Address("
-					+ "Email TEXT NOT NULL, "
-					+ "StreetAdd TEXT NOT NULL, "
-					+ "City TEXT NOT NULL, "
-					+ "State TEXT NOT NULL, "
-					+ "PostCode TEXT NOT NULL, "
-					+ "PRIMARY KEY (Email));";
-			cont.prepareStatement(sql);
-			cont.runSQLUpdate();
-
 			sql="INSERT INTO Employee(Name,ContactNo,Email) " +
 					"Values(?,?,?);";
 			cont.prepareStatement(sql);
@@ -60,60 +66,108 @@ public class AddEmployeeTest {
 
 			sql="INSERT INTO Address(EmployeeEmail,StreetAddress,City,State,PostCode) " 
 					+"Values(?,?,?,?,?) ;";
+
 			cont.prepareStatement(sql);
 			cont.getState().setString(1, "jgsons@gmail.com");
 			cont.getState().setString(2, "123 Fake Street");
 			cont.getState().setString(3, "Melbourne");
 			cont.getState().setString(4, "Victoria");
 			cont.getState().setString(5, "3000");
-
-			cont.closeConnection();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass(){
-		DatabaseModel mod = new DatabaseModel();
-		DatabaseController cont = new DatabaseController(mod);
-		String sql;
-		try{
-			cont.createConnection();
-			sql="DROP TABLE IF EXISTS Employee; DROP TABLE IF EXISTS Address;";
-			cont.prepareStatement(sql);
 			cont.runSQLUpdate();
+
 			cont.closeConnection();
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	//	@AfterClass
+	//	public static void tearDownAfterClass(){
+	//		DatabaseModel mod = new DatabaseModel();
+	//		DatabaseController cont = new DatabaseController(mod);
+	//		String sql;
+	//		try{
+	//			cont.createConnection();
+	//			sql="DROP TABLE IF EXISTS Employee; DROP TABLE IF EXISTS Address;";
+	//			cont.prepareStatement(sql);
+	//			cont.runSQLUpdate();
+	//			cont.closeConnection();
+	//		}catch (Exception e) {
+	//			e.printStackTrace();
+	//		}
+	//	}
+	//
 	@Test
 	public void testAddEmp(){
-		DatabaseModel mod = new DatabaseModel();
-		DatabaseController cont = new DatabaseController(mod);
+		//Can we double check this?
+		DatabaseModel dbmod = new DatabaseModel();
+		DatabaseController dbcont = new DatabaseController(dbmod);
 		String sql,name="",num="",email="",add="",city="",state="",postc="";
 		ResultSet res;
-		
+
 		EmployeeController econt = new EmployeeController();
 
 		try{
 			econt.addEmployee("John Smith", "0555 555 555","myemail@gmail.com","123 Fake Street", 
 					"Melbourne", "Victoria", "3000");
-			sql="SELECT * FROM Employee WHERE email='myemail@gmail.com';";
-			
-			
-			
+			sql="SELECT * FROM EMPLOYEE WHERE name='John Smith';";
+			dbcont.prepareStatement(sql);
+			res = dbcont.runSQLRes();
 
-			assertEquals(name,"Jacob Genericson");
+			name = res.getString("Name");
+			num = res.getString("ContactNo");
+			email = res.getString("Email");
+			add = res.getString("StreetAddress");
+			city = res.getString("City");
+			state = res.getString("State");
+			postc = res.getString("PostCode");
+
+			assertEquals(name,"John Smith");
+			assertEquals(num,"0555 555 555");
+			assertEquals(email,"myemail@gmail.com");
+			assertEquals(add,"123 Fake Street");
+			assertEquals(city,"Melbourne");
+			assertEquals(state,"Victoria");
+			assertEquals(postc,"3000");
+
 		}catch(Exception e){
 			fail("SQLException Error.\nTest Failure.\nStack Trace: ");
 			e.printStackTrace();
 		}
 	}
 
+	@Test
+	public void testCheckEmp(){
+		DatabaseModel dbmod = new DatabaseModel();
+		DatabaseController dbcont = new DatabaseController(dbmod);
+		String sql,name="",num="",email="",add="",city="",state="",postc="";
+		ResultSet res;
 
+		try{	
 
+			sql="SELECT * FROM EMPLOYEE WHERE name='Jacob Genericson';";
 
+			dbcont.prepareStatement(sql);
+			res = dbcont.runSQLRes();
+
+			name = res.getString("Name");
+			num = res.getString("ContactNo");
+			email = res.getString("Email");
+			add = res.getString("StreetAddress");
+			city = res.getString("City");
+			state = res.getString("State");
+			postc = res.getString("PostCode");
+
+			assertEquals(name,"Jacob Genericson");
+			assertEquals(num,"0555 555 555");
+			assertEquals(email,"myemail@gmail.com");
+			assertEquals(add,"123 Fake Street");
+			assertEquals(city,"Melbourne");
+			assertEquals(state,"Victoria");
+			assertEquals(postc,"3000");
+		}catch(Exception e){
+			fail("SQLException Error.\nTest Failure.\nStack Trace: ");
+			e.printStackTrace();
+		}
+	}
 }
