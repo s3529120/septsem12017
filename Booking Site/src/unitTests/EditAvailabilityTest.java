b13be4143c44a6758c55afdd05612461fd374c2a
RobLW <s3433096@student.rmit.edu.au>
2017-04-03 13:59:49 +1000

    Create setup and tear down methods

--- Booking Site/src/unitTests/EditAvailabilityTest.java
+++ Booking Site/src/unitTests/EditAvailabilityTest.java
@@ -1,5 +1,20 @@
 package unitTests;
 
+import Controller.DatabaseController;
+import Model.DatabaseModel;
+
 public class EditAvailabilityTest {
 
+	
+	public void setUpBeforeClass(){
+		DatabaseModel dbmod = new DatabaseModel();
+		DatabaseController dbcont = new DatabaseController(dbmod);
+		String sql="";
+		
+		sql = "CREATE TABLE IF DOES NOT EXIST Availibility;";
+	}
+	
+	public void tearDownAfterClass(){
+		
+	}
 }
