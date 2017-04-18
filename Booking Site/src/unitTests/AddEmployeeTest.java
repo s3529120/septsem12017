f3d30c1de7ef34283e7dd45686ce1fad882ef7cb
RobLW <s3433096@student.rmit.edu.au>
2017-04-18 16:22:53 +1000

    Add 15 more addEmp test cases

--- Booking Site/src/unitTests/AddEmployeeTest.java
+++ Booking Site/src/unitTests/AddEmployeeTest.java
@@ -226,9 +226,194 @@ public class AddEmployeeTest {
 		try{
 			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
 			
-			System.out.println(econt.getEmployeeName(email).length());
 			/* Get the employee name assigned to email, and compare length with some known value. */
 			assertEquals(econt.getEmployeeName(email).length(), name.length());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpInvalidEmail(){
+		EmployeeController econt = new EmployeeController();
+		String name = "Jhn InvalidEmail", contactno="0555 555 555", email="notanemail", 
+				streetadd="123 fake st", city="Melbourne", state="VIC", postcode="3000";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			assertFalse(econt.getEmail(name).endsWith("@email.com"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpEmailTooLong(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		int EMAIL_MAX_LENGTH = 25;
+		String name = "Jhn InvalidEmail", contactno="0555 555 555", email="aaaaaaaaaaaaaaaaaaaaaaaaa"
+				+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+				+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@email.com", 
+				streetadd="123 fake st", city="Melbourne", state="VIC", postcode="3000";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			assertFalse(econt.getEmail(name).length() < EMAIL_MAX_LENGTH);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpNoContactNo(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "ET NoPhone", contactno="", email="nophone@email.com", 
+				streetadd="123 fake st", city="Melbourne", state="VIC", postcode="3000";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			/* Should return no employee if some fields are missing */
+			assertFalse(econt.checkEmployee(email));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpContactNoTooLong(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "Phone TooLong", contactno="55555555555555555555555555555555555555"
+				+ "555555555555555555555555555555555555555555555555555555555555555555555", 
+				email="phoneTooLong@email.com", 
+				streetadd="123 fake st", city="Melbourne", state="VIC", postcode="3000";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			/* Should return false if some fields are invalid */
+			assertFalse(econt.checkEmployee(email));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpAddressTooLong(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "Address TooLong", contactno="0123456789", 
+				email="AddressTooLong@email.com", 
+				streetadd="12322222222222222222222222222222222222222222222222222222222"
+						+ "22222222222222222222222222222222222222222222222222222222222"
+						+ " fake st", city="Melbourne", state="VIC", postcode="3000";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			/* Should return false if some fields are invalid */
+			assertFalse(econt.checkEmployee(email));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpNoCity(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "No City", contactno="0123456789", 
+				email="NoCity@email.com", 
+				streetadd="123 fake st", city="", state="VIC", postcode="3000";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			/* Should return false if some fields are invalid */
+			assertFalse(econt.checkEmployee(email));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpNoState(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "No State", contactno="0123456789", 
+				email="NoState@email.com", 
+				streetadd="123 fake st", city="Melbourne", state="", postcode="3000";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			/* Should return false if some fields are invalid */
+			assertFalse(econt.checkEmployee(email));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpNoPostC(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "No Post", contactno="0123456789", 
+				email="NoPost@email.com", 
+				streetadd="123 fake st", city="Melbourne", state="VIC", postcode="";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			/* Should return false if some fields are invalid */
+			assertFalse(econt.checkEmployee(email));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpInvalidPostC(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "Inv Post", name2="inv Post2", contactno="0123456789", 
+				email="InvPost@email.com", email2 = "InvPost2@email.com", 
+				streetadd="123 fake st", city="Melbourne", state="VIC", postcode="abc", postcode2 = "0 221 2";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			econt.addEmployee(name2, contactno, email2, streetadd, city, state, postcode2);
+			
+			/* Should return false if any fields are invalid */
+			//has letters in postc
+			assertFalse(econt.checkEmployee(email));
+			//invalid numbers
+			assertFalse(econt.checkEmployee(email2));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testAddEmpPostCTooLong(){
+		EmployeeController econt = new EmployeeController();
+		// Some arbitrary length 
+		String name = "PostC TooLong", contactno="0123456789", 
+				email="PostCTooLong@email.com", 
+				streetadd="123 fake st", city="Melbourne", state="VIC", 
+				postcode="2222222222222222222222222222222222222222222222222222222222222222222222"
+						+ "222222222222222222222222222222222222222222222222222222222222222222222";
+		
+		try{
+			econt.addEmployee(name, contactno, email, streetadd, city, state, postcode);
+			
+			/* Should return false if some fields are invalid */
+			assertFalse(econt.checkEmployee(email));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
