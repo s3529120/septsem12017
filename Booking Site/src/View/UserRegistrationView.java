4d88bca464648544ff25f23e42755ef262ef88a2
Jasmine <ellis.jas48@gmail.com>
2017-04-30 21:45:20 +1000

    Add javadoc comments to UserRegistrationView

--- Booking Site/src/View/UserRegistrationView.java
+++ Booking Site/src/View/UserRegistrationView.java
@@ -24,171 +24,165 @@ public class UserRegistrationView {
 	public Stage stage;
 	private UserRegistrationController cont;
 
-
-	/**Constructor, sets stage.
-    * @param stage Window to manipulate.
-    */
-	public UserRegistrationView(Stage stage){
-		this.stage=stage;
+	/**
+	 * Constructor, sets stage.
+	 * 
+	 * @param stage
+	 *            Window to manipulate.
+	 */
+	public UserRegistrationView(Stage stage) {
+		this.stage = stage;
 	}
 
-	/**Sets associated controller
-    * @param cont Controller to associate.
-    * @return True upon success.
-    */
-	public Boolean setController(UserRegistrationController cont){
-		this.cont=cont;
+	/**
+	 * Sets associated controller
+	 * 
+	 * @param cont
+	 *            Controller to associate.
+	 * @return True upon success.
+	 */
+	public Boolean setController(UserRegistrationController cont) {
+		this.cont = cont;
 		return true;
 	}
 
-	/**Updates associated window.
-    */
-	public void updateView(){
-		
-		//Heading
+	/**
+	 * Updates associated window.
+	 */
+	public void updateView() {
+
+		// Heading
 		Text logtxt = new Text("Already have an account?");
-		
-		//Login button
+
+		// Login button
 		Button loginbtn = new Button("Login");
-		loginbtn.setOnAction(new EventHandler<ActionEvent>(){
-			@Override public void handle(ActionEvent e){
+		loginbtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
 				LoginController cont = new LoginController(new LoginView(stage));
 				cont.updateView();
 			}
 		});
-		
-		//Return button
+
+		// Return button
 		Button returnbtn = new Button("Return");
-		returnbtn.setOnAction(new EventHandler<ActionEvent>(){
-			@Override public void handle(ActionEvent e){		        
+		returnbtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
 				DefaultController cont = new DefaultController(stage, new MainMenuView(stage));
 				cont.updateView();
 			}
 		});
-		
-		VBox logbox = new VBox(logtxt,loginbtn);
-		
-		//Heading
+
+		VBox logbox = new VBox(logtxt, loginbtn);
+
+		// Heading
 		Text regtxt = new Text("Register with us.");
-		
-		
-		//Username
+
+		// Username
 		TextField usertxtfield = new TextField();
 		usertxtfield.setPromptText("Username");
 		HBox unamehbox = new HBox(usertxtfield);
 		unamehbox.setId("form");
 
-		//Name
+		// Name
 		TextField pnametxtfield = new TextField();
 		pnametxtfield.setPromptText("Full Name");
 		HBox pnamehbox = new HBox(pnametxtfield);
 		pnamehbox.setId("form");
 
-		//Address
+		// Address
 		TextField addtxtfield = new TextField();
 		addtxtfield.setPromptText("Address");
 		HBox addhbox = new HBox(addtxtfield);
 		addhbox.setId("form");
 
-		//Contact number
+		// Contact number
 		TextField numtxtfield = new TextField();
 		numtxtfield.setPromptText("Contact Number");
 		HBox numhbox = new HBox(numtxtfield);
 		numhbox.setId("form");
 
-		//Email address
+		// Email address
 		TextField mailtxtfield = new TextField();
 		mailtxtfield.setPromptText("Email Address");
 		HBox mailhbox = new HBox(mailtxtfield);
 		mailhbox.setId("form");
 
-		//Password
+		// Password
 		PasswordField pwordfield = new PasswordField();
 		pwordfield.setPromptText("Password");
 		HBox pwordhbox = new HBox(pwordfield);
 		pwordhbox.setId("form");
-		
-		//Confirm password
+
+		// Confirm password
 		PasswordField pwordfieldcon = new PasswordField();
 		pwordfieldcon.setPromptText("Confirm Password");
 		HBox pwordhboxcon = new HBox(pwordfieldcon);
 		pwordhboxcon.setId("form");
-		
-		//Error boxes
-		
-		//Unfilled box
+
+		// Error boxes
+
+		// Unfilled box
 		Text emptyerrortxt = new Text("All fields must be filled");
 		HBox emptyerrorbox = new HBox();
-	
-		
-		//Password mismatch
+
+		// Password mismatch
 		Text passerrortxt = new Text("Entered passwords do not match");
 		HBox passerrorbox = new HBox();
-				
-		//Username taken
+
+		// Username taken
 		Text unameerrortxt = new Text("That username is unavailable");
 		HBox unameerrorbox = new HBox();
-	
-		//invalid name
+
+		// invalid name
 		Text pnamerrortxt = new Text("Please enter a name using only letters, spaces, and hyphens");
 		HBox pnameerrorbox = new HBox();
-		
-		//Invalid email
+
+		// Invalid email
 		Text emailerrortxt = new Text("Please enter an email in the correct format");
 		HBox emailerrorbox = new HBox();
-		
-		//Phone too long
+
+		// Phone too long
 		Text phoneerrortxt = new Text("Please enter a valid phone number only 10 digits long");
 		HBox phoneerrorbox = new HBox();
-		
+
 		// Street address invalid
 		Text streeterrortxt = new Text("Please enter a street number followed by a street name");
 		HBox streeterrorbox = new HBox();
-		
-		//Register button
-		Button registerbtn = new Button("Register");
-		registerbtn.setOnAction(new EventHandler<ActionEvent>(){
-			@Override public void handle(ActionEvent e){
 
-				//checking to make sure all fields are filled
-			   if(cont.checkValues(usertxtfield,
-			                    pnametxtfield,pwordfield,pwordfieldcon,
-			                    numtxtfield,addtxtfield,mailtxtfield)){
-					cont.register(usertxtfield.getText(),
-							pnametxtfield.getText(),pwordfield.getText(),
-							numtxtfield.getText(),addtxtfield.getText(),mailtxtfield.getText());
-			   }else{
-			      //checking for empty
-				   cont.validateEntries(
-						   usertxtfield, unamehbox, 
-							pnametxtfield, pnamehbox, 
-							pwordfield, pwordhbox, 
-							pwordfieldcon, pwordhboxcon, 
-							addtxtfield, addhbox, 
-							numtxtfield, numhbox, 
-							mailtxtfield, mailhbox, 
-							emptyerrortxt, emptyerrorbox,
-							passerrortxt, passerrorbox,
-							unameerrortxt, unameerrorbox,
-							pnamerrortxt, pnameerrorbox,
-							emailerrortxt, emailerrorbox,
-							phoneerrortxt, phoneerrorbox,
-							streeterrortxt, streeterrorbox);
-			   }
+		// Register button
+		Button registerbtn = new Button("Register");
+		registerbtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+
+				// checking to make sure all fields are filled
+				if (cont.checkValues(usertxtfield, pnametxtfield, pwordfield, pwordfieldcon, numtxtfield, addtxtfield,
+						mailtxtfield)) {
+					cont.register(usertxtfield.getText(), pnametxtfield.getText(), pwordfield.getText(),
+							numtxtfield.getText(), addtxtfield.getText(), mailtxtfield.getText());
+				} else {
+					// checking for empty
+					cont.validateEntries(usertxtfield, unamehbox, pnametxtfield, pnamehbox, pwordfield, pwordhbox,
+							pwordfieldcon, pwordhboxcon, addtxtfield, addhbox, numtxtfield, numhbox, mailtxtfield,
+							mailhbox, emptyerrortxt, emptyerrorbox, passerrortxt, passerrorbox, unameerrortxt,
+							unameerrorbox, pnamerrortxt, pnameerrorbox, emailerrortxt, emailerrorbox, phoneerrortxt,
+							phoneerrorbox, streeterrortxt, streeterrorbox);
+				}
 			}
 		});
 		HBox btnbox = new HBox(registerbtn);
-		
-		//Layout
-		
-		VBox vbox = new VBox(returnbtn,regtxt,unamehbox,pnamehbox,addhbox,numhbox,
-		                     mailhbox,pwordhbox, pwordhboxcon,btnbox,emptyerrorbox,
-		                     passerrorbox,unameerrorbox,pnameerrorbox,emailerrorbox,
-		                     phoneerrorbox,streeterrorbox);
-		
-		HBox regmenubox = new HBox(vbox,logbox);
-		
-		//Styles
+
+		// Layout
+
+		VBox vbox = new VBox(returnbtn, regtxt, unamehbox, pnamehbox, addhbox, numhbox, mailhbox, pwordhbox,
+				pwordhboxcon, btnbox, emptyerrorbox, passerrorbox, unameerrorbox, pnameerrorbox, emailerrorbox,
+				phoneerrorbox, streeterrorbox);
+
+		HBox regmenubox = new HBox(vbox, logbox);
+
+		// Styles
 		regmenubox.getStyleClass().add("loginpageBox");
 		logbox.getStyleClass().add("vbox");
 		vbox.getStyleClass().add("regbox");
@@ -197,7 +191,7 @@ public class UserRegistrationView {
 		loginbtn.setId("largebtn");
 		regtxt.setId("heading");
 		logtxt.setId("heading");
-		
+
 		StackPane pane = new StackPane(regmenubox);
 
 		Scene scene = new Scene(pane);
