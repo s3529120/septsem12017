da4fc315d02a5516d960ae7decde85362e474530
Jasmine <ellis.jas48@gmail.com>
2017-05-04 15:51:37 +1000

    Improve layout for login.

--- Booking Site/src/View/LoginView.java
+++ Booking Site/src/View/LoginView.java
@@ -5,10 +5,13 @@ import Controller.LoginController;
 import Controller.UserRegistrationController;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -47,7 +50,6 @@ public class LoginView {
 	public void updateView() {
 
 		// Register button
-		Text regtxt = new Text("Register with us.");
 		Button registerbtn = new Button("Register");
 		registerbtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -69,7 +71,6 @@ public class LoginView {
 		});
 
 		// Register box
-		VBox regbox = new VBox(regtxt, registerbtn);
 
 		// Welcome text
 		Text welcometxt = new Text("Welcome back!");
@@ -78,21 +79,27 @@ public class LoginView {
 		TextField usernamefield = new TextField();
 		HBox usernameBox = new HBox();
 		usernamefield.setPromptText("Username");
+		usernamefield.setId("form");
 		usernameBox.getChildren().add(usernamefield);
+		usernameBox.setAlignment(Pos.CENTER);
 
 		// Password field
 		PasswordField pwordfield = new PasswordField();
 		HBox pwordBox = new HBox();
 		pwordfield.setPromptText("Password");
+		pwordfield.setId("form");
 		pwordBox.getChildren().add(pwordfield);
+		pwordBox.setAlignment(Pos.CENTER);
 
 		// Empty error box
 		Text emptyerrortxt = new Text("All fields must be filled");
 		HBox emptyerrorbox = new HBox();
+		emptyerrorbox.setAlignment(Pos.CENTER);
 
 		// Incorrect entry box
 		Text failerrortxt = new Text("Incorrect username and password combination");
 		HBox failerrorbox = new HBox();
+		failerrorbox.setAlignment(Pos.CENTER);
 
 		// Login button
 		Button loginbtn = new Button("Login");
@@ -106,8 +113,8 @@ public class LoginView {
 						}
 						cont.validateEntries(usernamefield, usernameBox, pwordfield, pwordBox, emptyerrortxt,
 								emptyerrorbox);
-						usernameBox.setId("form");
-						pwordBox.setId("form");
+						usernamefield.setId("form");
+						pwordfield.setId("form");
 					}
 				} else {
 					cont.validateEntries(usernamefield, usernameBox, pwordfield, pwordBox, emptyerrortxt,
@@ -120,26 +127,37 @@ public class LoginView {
 		});
 
 		// Layout
+		BorderPane border = new BorderPane();
+		VBox body = new VBox(15, welcometxt, usernameBox, pwordBox, loginbtn, failerrorbox, emptyerrorbox, registerbtn);
+		
+		BorderPane.setAlignment(body, Pos.CENTER);
+		BorderPane.setMargin(body, new Insets(12, 12, 12, 12));
+		border.setCenter(body);
+		
+		HBox tophbox = new HBox();
+		tophbox.setPadding(new Insets(20, 12, 15, 12));
+		border.setTop(tophbox);
 
-		VBox vbox = new VBox(returnbtn, welcometxt, usernameBox, pwordBox, loginbtn, failerrorbox, emptyerrorbox);
+		VBox leftvbox = new VBox();
+		leftvbox.setPadding(new Insets(15, 400, 15, 12));
+		border.setLeft(leftvbox);
 
-		HBox loginpageBox = new HBox(regbox, vbox);
+		VBox rightvbox = new VBox();
+		rightvbox.setPadding(new Insets(15, 400, 15, 12));
+		border.setRight(rightvbox);
+
+		HBox bottomhbox = new HBox();
+		bottomhbox.setPadding(new Insets(100, 12, 15, 12));
+		border.setBottom(bottomhbox);
 
 		// Styles
-		loginpageBox.getStyleClass().add("loginpageBox");
-		vbox.getStyleClass().add("vbox");
-		returnbtn.setId("loginbtn");
-		regbox.getStyleClass().add("regbox");
-		usernameBox.setId("form");
-		pwordBox.setId("form");
-		registerbtn.setId("registerbtn");
-		regtxt.setId("heading");
-		welcometxt.setId("heading");
-		loginbtn.setId("loginbtn");
+		welcometxt.getStyleClass().add("main-heading");
+		body.setId("mainMenuVBox");
+		border.setId("border");
+		registerbtn.getStyleClass().add("linkbtn");
+		loginbtn.getStyleClass().add("orangebtn");
 
-		StackPane pane = new StackPane(loginpageBox);
-
-		Scene scene = new Scene(pane);
+		Scene scene = new Scene(border);
 		scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
 		stage.setScene(scene);
 	}
