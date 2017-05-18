ea6fbc002fbd17702e481f45c597a01f19e7bcef
Jasmine <ellis.jas48@gmail.com>
2017-05-18 19:30:09 +1000

    Add business pop up.

--- Booking Site/src/admin/AdminView.java
+++ Booking Site/src/admin/AdminView.java
@@ -18,6 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -36,7 +37,6 @@ import service.TypeView;
 import utils.AppData;
 
 public class AdminView {
-
 
 	private ScrollPane sp;
 	private String currentdate;
@@ -58,6 +58,7 @@ public class AdminView {
 		this.cont = controller;
 		return true;
 	}
+
 	/**
 	 * Updates the associated view of the admin dashboard
 	 */
@@ -69,7 +70,7 @@ public class AdminView {
 
 		HBox bookingstitle = new HBox(h1);
 		bookingstitle.setId("bookingstitle");
-		
+
 		Button addbtn = new Button("Add Business");
 		addbtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -100,7 +101,8 @@ public class AdminView {
 		// Create a list of businesses and pass through each business to list
 		List<BusinessAccountModel> busList = cont.getBusinesses();
 		VBox businessList = new VBox();
-		// TODO: admincontroller.getBusinesses(). otherwise null reference error is returned
+		// TODO: admincontroller.getBusinesses(). otherwise null reference error
+		// is returned
 
 		busList.forEach(business -> {
 			// Display business data
@@ -116,7 +118,7 @@ public class AdminView {
 					confirmDelete(business, cont);
 				}
 			});
-			
+
 			delBusBtn.setAlignment(Pos.CENTER_RIGHT);
 			delBusBtn.getStyleClass().add("delete-btn");
 
@@ -131,18 +133,17 @@ public class AdminView {
 			businessList.getChildren().add(busBox);
 		});
 
-
 		// Layout
 		h1.getStyleClass().add("main-heading");
 		header.setId("headerbox");
 		businessList.setId("bookings-main");
-		
+
 		ScrollPane body = new ScrollPane(businessList);
 		body.getStyleClass().add("scroll-pane");
 		body.setId("mainPageVBox");
 
 		VBox page = new VBox(header, businessList);
-		
+
 		page.setId("border");
 		page.getStyleClass().add("loginpageBox");
 
@@ -155,9 +156,10 @@ public class AdminView {
 		stage.setScene(scene);
 		stage.show();
 	}
-	
+
 	/**
 	 * Delete business confirmation pop up
+	 * 
 	 * @param business
 	 * @param adcont
 	 */
@@ -168,9 +170,9 @@ public class AdminView {
 		StackPane pane = new StackPane();
 
 		String busname = business.getBusinessName().toString();
-		
+
 		Text message = new Text("You are about to delete " + busname + ".\n Are you sure?");
-		
+
 		Button confirm = new Button("Confirm");
 		confirm.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 			@Override
@@ -207,32 +209,117 @@ public class AdminView {
 		popup.setScene(scene);
 		popup.show();
 	}
-	
+
 	public void addBusiness(AdminController cont) {
 		Stage popup = new Stage();
 		popup.initModality(Modality.WINDOW_MODAL);
 		popup.initOwner(cont.getView().stage);
 		StackPane pane = new StackPane();
-		
-		Text h1 = new Text("Add a Business");
-		
-		// Add business form
-//		String Busname,String Password,String Name,
-//		String ContactNo,String Address, String Email
-		
-		// Business name
-		TextField busnametxtfield = new TextField();
-		busnametxtfield.setPromptText("Business name");
-		busnametxtfield.setId("form");
-		busnametxtfield.setAlignment(Pos.CENTER);
-		
-		// Add to pane
-		//VBox all = new VBox(20, message, buttons);
-		//buttons.setAlignment(Pos.CENTER);
-		//message.setTextAlignment(TextAlignment.CENTER);
-		//pane.getChildren().addAll(all);
 
-		// Set scene for making booking
+		Text h1 = new Text("Add a Business");
+
+		// Add business form
+
+		// Username
+		TextField usertxtfield = new TextField();
+		usertxtfield.setPromptText("Username");
+		HBox unamehbox = new HBox(usertxtfield);
+		usertxtfield.setId("form");
+		// unamehbox.setAlignment(Pos.CENTER);
+
+		// Name
+		TextField pnametxtfield = new TextField();
+		pnametxtfield.setPromptText("Business Name");
+		HBox pnamehbox = new HBox(pnametxtfield);
+		pnametxtfield.setId("form");
+		// pnamehbox.setAlignment(Pos.CENTER);
+
+		// Address
+		TextField addtxtfield = new TextField();
+		addtxtfield.setPromptText("Address");
+		HBox addhbox = new HBox(addtxtfield);
+		addtxtfield.setId("form");
+		// addhbox.setAlignment(Pos.CENTER);
+
+		// Contact number
+		TextField numtxtfield = new TextField();
+		numtxtfield.setPromptText("Contact Number");
+		HBox numhbox = new HBox(numtxtfield);
+		numtxtfield.setId("form");
+		// numhbox.setAlignment(Pos.CENTER);
+
+		// Email address
+		TextField mailtxtfield = new TextField();
+		mailtxtfield.setPromptText("Email Address");
+		HBox mailhbox = new HBox(mailtxtfield);
+		mailtxtfield.setId("form");
+		// mailhbox.setAlignment(Pos.CENTER);
+
+		// Password
+		PasswordField pwordfield = new PasswordField();
+		pwordfield.setPromptText("Password");
+		HBox pwordhbox = new HBox(pwordfield);
+		pwordfield.setId("form");
+		// pwordhbox.setAlignment(Pos.CENTER);
+
+		// Confirm password
+		PasswordField pwordfieldcon = new PasswordField();
+		pwordfieldcon.setPromptText("Confirm Password");
+		HBox pwordhboxcon = new HBox(pwordfieldcon);
+		pwordfieldcon.setId("form");
+		// pwordhboxcon.setAlignment(Pos.CENTER);
+		VBox dets = new VBox(unamehbox, pnamehbox, addhbox, mailhbox, numhbox, pwordhbox, pwordhboxcon);
+
+		// Cancel button
+		Button cancel = new Button("Cancel");
+		cancel.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				popup.close();
+			}
+		});
+		cancel.getStyleClass().add("orangebtn-small");
+
+		Button submit = new Button("Submit");
+		submit.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				if (cont.addBusiness(usertxtfield.getText().toString(), pwordfield.getText().toString(),
+						pnametxtfield.getText().toString(), numtxtfield.getText().toString(),
+						addtxtfield.getText().toString(), mailtxtfield.getText().toString())) {
+					
+					// Confirm text
+					Text conftxt = new Text("Add business Confirmed!");
+					// Close button
+					Button close = new Button("Close");
+					close.setOnAction(new EventHandler<ActionEvent>() {
+						@Override
+						public void handle(ActionEvent e) {
+							popup.close();
+							cont.updateView();
+						}
+					});
+					close.getStyleClass().add("orangebtn-small");
+					VBox vbox = new VBox(conftxt, close);
+					vbox.setId("pop-up");
+					Scene confscene = new Scene(vbox);
+					confscene.getStylesheets()
+							.add(getClass().getResource("/resources/display/css/styles.css").toExternalForm());
+					popup.setScene(confscene);
+					cont.updateView();
+				} else {
+					// False message
+				}
+			}
+		});
+		submit.getStyleClass().add("orangebtn-small");
+
+		HBox buttons = new HBox(10, submit, cancel);
+
+		VBox all = new VBox(15, h1, dets, buttons);
+
+		// Set scene for add business
+		pane.getChildren().addAll(all);
 		pane.setId("pop-up");
 		Scene scene = new Scene(pane);
 		scene.getStylesheets().add(getClass().getResource("/resources/display/css/styles.css").toExternalForm());
