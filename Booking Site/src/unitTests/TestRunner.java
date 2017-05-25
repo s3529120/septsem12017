5861597f042ed1fd70a660203297917927e55e88
Rob <rlainewyllie@gmail.com>
2017-05-25 23:52:59 +1000

    Rearrange pom file for dependency of junit in test scope

--- Booking Site/src/unitTests/TestRunner.java
+++ Booking Site/src/unitTests/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
 	public static void main(String[] args) {
-
+		
 		Result result = JUnitCore.runClasses(TestSuite.class);
 
 		for (Failure failure : result.getFailures()) {
--- Booking Site/src/unitTests/TestSuite.java
+++ Booking Site/src/unitTests/TestSuite.java
@@ -3,7 +3,6 @@ package unitTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
